ebe2ab883a00e6644570cccd97cf5ab27aa9cb0a
Ramon Vieira <ramonvieira122333@gmail.com>
2023-06-23 12:00:30 -0300

    atualizando as ilustrações dos benefícios

--- web/src/components/main/Card.tsx
+++ web/src/components/main/Card.tsx
@@ -2,12 +2,13 @@ interface cardProps {
   urlImg: string;
   title: string;
   paragraph: string;
+  top: number;
 }
 
 export function Card(props: cardProps) {
   return (
     <div className="max-w-[308px] bg-primary dark:bg-secondary rounded-2xl p-6 relative pt-44 flex flex-col items-center gap-5 mt-32">
-      <img src={props.urlImg} alt="" className="absolute -top-24" />
+      <img src={props.urlImg} alt="" className={`absolute -top-${props.top}`} />
       <h4 className="text-2xl font-bold">{props.title}</h4>
       <p className="text-xl">{props.paragraph}</p>
     </div>
--- web/src/components/positives/Positive.tsx
+++ web/src/components/positives/Positive.tsx
@@ -1,13 +1,15 @@
 import { Card } from "../main/Card";
-import ilustration from "../../assets/ilustration.png";
+import meninaVoando from "../../assets/meninaVoando.png";
+import meninaSurfando from "../../assets/meninaSurfando.png";
+import garota2 from "../../assets/garota2.png";
+import meninoLendo from "../../assets/meninoLendo.png";
+import menino2 from "../../assets/menino2.png";
+import menino3 from "../../assets/menino3.png";
 
 import { useTranslation } from "react-i18next";
-import { ChangeLanguage } from "../utils/ChangeLanguage";
-
-
 
 export function Positive() {
-  const {t}= useTranslation()
+  const { t } = useTranslation();
 
   return (
     <div className="mt-32 md:mt-36 lg:mt-52 2xl:mt-64 mb-20 py-6 px-6 max-w-5xl mx-auto ">
@@ -17,34 +19,40 @@ export function Positive() {
 
       <div className="flex flex-col md:flex-row items-center md:items-stretch justify-around flex-wrap gap-6 m-auto">
         <Card
-          urlImg={ilustration}
+          urlImg={meninaVoando}
           title={t("headerCardPosit1")}
           paragraph={t("textCardPosit1")}
+          top={36}
         />
         <Card
-          urlImg={ilustration}
+          urlImg={meninoLendo}
           title={t("headerCardPosit2")}
           paragraph={t("textCardPosit2")}
+          top={36}
         />
         <Card
-          urlImg={ilustration}
+          urlImg={garota2}
           title={t("headerCardPosit3")}
           paragraph={t("textCardPosit3")}
+          top={52}
         />
         <Card
-          urlImg={ilustration}
+          urlImg={menino2}
           title={t("headerCardPosit4")}
           paragraph={t("textCardPosit4")}
+          top={28}
         />
         <Card
-          urlImg={ilustration}
+          urlImg={meninaSurfando}
           title={t("headerCardPosit5")}
           paragraph={t("textCardPosit5")}
+          top={20}
         />
         <Card
-          urlImg={ilustration}
+          urlImg={menino3}
           title={t("headerCardPosit6")}
           paragraph={t("textCardPosit6")}
+          top={36}
         />
       </div>
     </div>
