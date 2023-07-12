8cd1d0196a51623b8375c94af86d5165de7cb564
José Vinicius Targino Muniz <20222014040010@ifrn.local>
2023-07-12 16:03:33 -0300

    corrigindo cor do sobre do header

--- web/src/components/header/Header.tsx
+++ web/src/components/header/Header.tsx
@@ -58,7 +58,7 @@ export function Header() {
               <li>
                 <Link
                   to="#"
-                  className="font-thin text-white hover:text-theader2 relative after:content-[''] after:block after:h-[2px] after:w-0  after:bg-theader2 dark:after:bg-white after:absolute after:-bottom-1 after:transition-all after:duration-300 hover:after:w-full"
+                  className="font-thin text-white hover:text-[#073344] relative after:content-[''] after:block after:h-[2px] after:w-0  after:bg-[#073344] dark:after:bg-white after:absolute after:-bottom-1 after:transition-all after:duration-300 hover:after:w-full"
                 >
                   {t("item3")}
                 </Link>
